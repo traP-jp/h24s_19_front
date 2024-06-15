cdc20f75b14f86d96ceeb4f190f6ab07da11fec3
shota <shotashiina132@gmail.com>
2024-06-15 23:47:32 +0900

    修正できてなかったエラーの修正

--- src/pages/RoomList.vue
+++ src/pages/RoomList.vue
@@ -10,27 +10,27 @@ onMounted(async () => {
 </script>
 
 <template>
-<div>
-  <div v-for="room in rooms" :key="room.roomId">
-    <div
-      class="roomListForms"
-      v-if="room.roomId && typeof(room.isPublic) == 'boolean'"
-    >
-      <!-- 下のpathは選択した部屋に入るページに飛ぶようにする -->
-      <button
-        @click="
-          () => {
-            $router.push({ path: '/room/' + room.roomId + '/enter' })
-          }
-        "
+  <div>
+    <div v-for="room in rooms" :key="room.roomId">
+      <div
+        v-if="room.roomId && typeof(room.isPublic) == 'boolean'"
+        class="roomListForms"
       >
-        <div v-if="room.isPublic">フリールーム</div>
-        <div v-if="!room.isPublic">プライベートルーム</div>
-        {{ room.roomName }}, {{ room.userCount }}人参加中
-      </button>
+        <!-- 下のpathは選択した部屋に入るページに飛ぶようにする -->
+        <button
+          @click="
+            () => {
+              $router.push({ path: '/room/' + room.roomId + '/enter' })
+            }
+          "
+        >
+          <div v-if="room.isPublic">フリールーム</div>
+          <div v-if="!room.isPublic">プライベートルーム</div>
+          {{ room.roomName }}, {{ room.userCount }}人参加中
+        </button>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <style scoped>
